eafa62980c1b1adf481ef92297dd5866c2a7c6a3
ahchoret <1771336146@qq.com>
2026-07-04 01:23:02 +0800

    backend16

--- api/account.go
+++ api/account.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	"database/sql"
-	"github.com/lllmml/simplebank/db/sqlc"
 	"net/http"
+
+	"github.com/lib/pq"
+	"github.com/lllmml/simplebank/db/sqlc"
 
 	"github.com/gin-gonic/gin"
 )
@@ -28,6 +30,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -8,8 +9,6 @@ import (
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 func init() {
 	rand.Seed(time.Now().UnixNano())
-
-
 }
 
 func RandomInt(min, max int64) int64{
@@ -20,7 +19,7 @@ func RandomString(n int) string{
 	var sb strings.Builder
 	k := len(alphabet)
 
-	for i:= 0; i< 0; i++ {
+	for i:= 0; i < n; i++ {
 		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
 	}
@@ -39,4 +38,9 @@ func RandomCurrency() string {
 	currencies := []string{EUR, USD, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
+}
+
+// RandomEmail generates a random email address.
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
