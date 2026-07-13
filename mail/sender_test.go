7aa3d0bd8832a272ebc5103d97a0e91eaaa3b9d5
ahchoret <1771336146@qq.com>
2026-07-13 21:49:01 +0800

    add -short for test #60

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -20,9 +20,9 @@ func TestSendEmailWithGmail(t *testing.T) {
 	subject := "A test email"
 	content := `
 	<h1>Hello world</h1>
-	<p>This is a test message from <a href="http://techschool.guru">Tech School</a></p>
+	<p>This is a test message from <a href="http://lllmml">Tech School</a></p>
 	`
-	to := []string{"techschool.guru@gmail.com"}
+	to := []string{"1771336146lml@gmail.com"}
 	attachFiles := []string{"../README.md"}
 
 	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
